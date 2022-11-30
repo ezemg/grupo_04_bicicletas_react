8fa0fde60d54ce629dfcd796af31a466a39185c6
ezemg <ezequielmatiasgr@gmail.com>
2022-11-30 13:57:01 -0300

    Ezequiel - 2022 11 30 - Armo repo react

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SideBar from './SideBar';
+import ContentWrapper from './ContentWrapper';
+import SearchMovies from './SearchMovies';
+function App() {
+  return (
+    <React.Fragment>
+      <div id="wrapper">
+        <SideBar />
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
